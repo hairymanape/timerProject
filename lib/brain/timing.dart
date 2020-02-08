02924b83b2c637e6d2d50ed0968014b860544eda
jacob <jacobgfoley@gmail.com>
2020-02-09 00:41:39 +1300

    Changed projectCard to be stateful Added a timer to projectCard Changed timer to be a plain of dart class PODO :p

--- lib/brain/timing.dart
+++ lib/brain/timing.dart
@@ -1,50 +1,74 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
-class Project {
-  String name;
-  int code;
-  int time;
 
-  Project(this.name, this.code, this.time);
-}
+/******************************* COMMENT FOR MATT *******************************/
+/// I changed this from being a widget to a plain dart class.
+/// It doesn't need to display itself and it was easier this way :p
 
-class TimerBrain extends StatefulWidget {
-  @override
-  _TimerBrainState createState() => _TimerBrainState();
-}
 
-class _TimerBrainState extends State<TimerBrain> {
+class TimerBrain extends ChangeNotifier {
   int elapsedMilliseconds;
   String stopTimeToDisplay = "_";
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(child: Text(stopTimeToDisplay));
+  bool _isRunning = false;
+  final Stopwatch _stopwatch = Stopwatch();
+  final dur = const Duration(seconds: 1);
+  // Added the ability for this class to take in a starting count
+  final startCount;
+
+  TimerBrain({this.startCount = 0}) {
+    _init();
   }
 
-  var swatch = Stopwatch();
-  final dur = const Duration(seconds: 1);
+  _init() {
+    if (this.startCount > 0) {
+      // Only display the starting count if it is above 0
+      _setTimeToDisplay();
+    }
+  }
+
+  _setTimeToDisplay() {
+    // Add the startCount to the elapsed time
+    stopTimeToDisplay =
+        (_stopwatch.elapsed.inSeconds + startCount).toString().padLeft(2, "0");
+  }
 
   void startTimer() {
     Timer(dur, keepRunning);
   }
 
   void keepRunning() {
-    if (swatch.isRunning) {
+    if (_stopwatch.isRunning) {
       startTimer();
     }
-    setState(() {
-      stopTimeToDisplay = (swatch.elapsed.inSeconds).toString().padLeft(2, "0");
-    });
+    _setTimeToDisplay();
+
+    // This will notify any widgets who are using the timer that something has changed and that they need to re render
+    notifyListeners();
   }
 
   void startStopWatch() {
-    swatch.start();
+
+    // this likely isn't required but is just an extra check to avoid calling start if it is already started
+    if (_isRunning) {
+      return;
+    }
+
+    _isRunning = true;
+    _stopwatch.start();
     startTimer();
   }
 
   void stopStopWatch() {
-    swatch.stop();
+    _isRunning = false;
+    _stopwatch.stop();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    // TODO: check if we need to dispose these timers
+    super.dispose();
   }
 }
